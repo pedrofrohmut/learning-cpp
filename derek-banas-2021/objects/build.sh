4f91d51fc0a27ac7be9e8049443ecbf1fc374d24
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-09-03 12:06:10 -0300

    Clean Up Compiled and Executables

--- derek-banas-2021/objects/build.sh
+++ derek-banas-2021/objects/build.sh
@@ -4,9 +4,11 @@
 # -I the header files directory for includes
 # -o for creating output files
 
+mkdir ./bin
+
 g++ -c ./src/Shape.cpp -I include/ -o ./bin/Shape.o
 g++ -c ./src/Circle.cpp -I include/ -o ./bin/Circle.o
 g++ -c ./src/main.cpp -I include/ -o ./bin/main.o
 
 # link all in a single executable called main
-g++ ./bin/Shape.o ./bin/Circle.o ./bin/main.o -o main
+g++ ./bin/Shape.o ./bin/Circle.o ./bin/main.o -o main.out
