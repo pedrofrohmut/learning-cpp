505493ba38c8810ad0dae2706b5ac06d5c7f3ffe
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-02-07 14:52:30 -0300

    Pong ball bounces around

--- programming-with-nick/pong/src/main.cpp
+++ programming-with-nick/pong/src/main.cpp
@@ -10,17 +10,22 @@ public:
     float speed_x, speed_y;
     float radius;
 
-    void draw() {
+    void draw()
+    {
         DrawCircle(x, y, radius, ORANGE);
     }
 
-    void update() {
+    void update(int win_width, int win_height)
+    {
+        if (x + radius >= win_width || x - radius <= 0) speed_x *= -1;
+        if (y + radius >= win_height || y - radius <= 0) speed_y *= -1;
         x += speed_x;
         y += speed_y;
     }
 };
 
-int main() {
+int main()
+{
     // Initialization
     const int width = 1280;
     const int height = 800;
@@ -32,17 +37,14 @@ int main() {
     ball.radius = 20;
     ball.x = (float) width / 2;
     ball.y = (float) height / 2;
-    ball.speed_x = 3;
-    ball.speed_y = 0;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
 
     while (!WindowShouldClose()) {
         // Update
-        if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_Q)) {
-            std::cout << "CTRL-Q" << std::endl;
-            break;
-        }
+        if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_Q)) break;
 
-        ball.update();
+        ball.update(width, height);
 
         // Draw
         BeginDrawing();
@@ -51,12 +53,10 @@ int main() {
         ball.draw();
 
         DrawRectangle(35, height/2 - 60, 25, 120, ORANGE);
-
         DrawRectangle(width - 60, height/2 - 60, 25, 120, ORANGE);
-
         DrawLine(width/2, 0, width/2, height, ORANGE);
 
-        DrawLine(0, height/2, width, height/2, ORANGE);
+        // DrawLine(0, height/2, width, height/2, ORANGE); // Dev ref (comment for prod)
 
         EndDrawing();
     }
