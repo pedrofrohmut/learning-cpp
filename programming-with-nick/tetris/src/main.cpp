7290dee6dcabce20b1b5924b60fc81461c7dd5d2
pedrofrohmut <pedrofrohmut@gmail.com>
2024-11-23 12:35:02 -0300

    Raylib Tetris - hello world

---
+++ programming-with-nick/tetris/src/main.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main(void)
+{
+    InitWindow(800, 600, "Hello, World!");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RED);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
